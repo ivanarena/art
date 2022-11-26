c9d9f6de20f4d293d4fcb2be357c1a70e92fe767
ivanarena <ivan.x28x@gmail.com>
2022-11-26 18:59:32 +0100

    made works section

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-
+// nav anchors
 const main = document.getElementById('main-link');
 const works = document.getElementById('works-link');
 const about = document.getElementById('about-link');
 const contact = document.getElementById('contact-link');
+let activeAnchor = main;
 
+// text sections
 const mainText = document.getElementById('main-text');
 const worksText = document.getElementById('works-text');
 const aboutText = document.getElementById('about-text');
 const contactText = document.getElementById('contact-text');
+let activeText = mainText;
 
-let activeAnchor = main;
-let activeSection = mainText;
+const worksSection = document.getElementById('works-sec');
+let activeSection = worksSection;
+
 
 function setActive(e) {
     if (e.target != activeAnchor) {
         activeAnchor.classList.remove('is-active');
+        activeText.classList.remove('is-active');
         activeSection.classList.remove('is-active');
 
         activeAnchor = e.target;
         activeAnchor.classList.add('is-active')
 
         if (activeAnchor == main) {
-            activeSection = mainText;
+            activeText = mainText;
             mainText.classList.add('is-active');
         } else if (activeAnchor == works) {
-            activeSection = worksText;
+            activeText = worksText;
+            activeSection = worksSection;
             worksText.classList.add('is-active');
+            worksSection.classList.add('is-active');
         } else if (activeAnchor == about) {
-            activeSection = aboutText;
+            activeText = aboutText;
             aboutText.classList.add('is-active');
         } else if (activeAnchor == contact) {
-            activeSection = contactText;
+            activeText = contactText;
             contactText.classList.add('is-active');
         }
     }
